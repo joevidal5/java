c636d9b356ef51e1f2b4aebb4178a8cec7eb449b
Marwos97 <marwos97@gmail.com>
2020-05-20 20:50:04 -0300

    Base de datos arreglada

--- js/registro.js
+++ js/registro.js
@@ -1,29 +1,41 @@
-let myStorage = window.localStorage;
 /* myStorage.setItem('datajson',JSON.stringify(registro)); */
 let registrados = []; 
 function RegistroUsuario(){
-    let usuario = document.getElementById("usuario").value;
-    let contrasenia = document.getElementById("contrasenia").value;
+    var esta = false;
+    let usuario = document.getElementById("user").value;
+    let contrasenia = document.getElementById("password").value;
     let mail = document.getElementById("mail").value;
-    console.log('Registro Completo');
+    registrados = myStorage.getItem('datajson');
+    if (registrados){
+        registrados = JSON.parse(registrados);
+    }
     let registrado = {
         username: usuario,
         password: contrasenia,
         email: mail,
+        
     }
-    registrados.push(registrado);
-    myStorage.setItem('datasson', JSON.stringify(registrados));
+
+    for (var i = 0; i<registrados.length; i++){
+        console.log(i);
+        console.log('gola');
+        if (usuario == registrados[i]['username'] || mail == registrados[i]['email']) {
+            alert('Este usuario se encuentra registrado');
+            esta = true;
+        }
+    }
+
+    if (!esta){
+        registrados.push(registrado);
+        myStorage.setItem('datajson', JSON.stringify(registrados));
+        console.log('update')
+        console.log(registrados)
+    }
+    
+    
 }
 
 
-let Pagina = {
-    alert("gola")
-    _countRegistros: 0,
-    _registrados: [],
-    
-    
-    
-}
 
 
 
