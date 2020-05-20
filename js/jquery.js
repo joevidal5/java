fb9a2951b416abe8fccfaad457dd977f68ef5b54
joelvidal <j.vidal@itecriocuarto.org.ar>
2020-05-20 17:30:10 -0300

    acomodando id

---
+++ js/jquery.js
@@ -0,0 +1,7 @@
+function mostrar(ocultarPag){
+    document.getElementById(ocultarPag).style.display="block";
+}
+
+function ocultar(ocultarPag){
+    document.getElementById(ocultarPag).style.display="none";
+}
