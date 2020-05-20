01ed54a73116e3e8b575e3840306e5fd7b9047fc
Marwos97 <marwos97@gmail.com>
2020-05-20 19:35:20 -0300

    renegando

--- js/registro.js
+++ js/registro.js
@@ -1,31 +1,19 @@
 let myStorage = window.localStorage;
-let registro = {
-    hola: "temana",
-    chau: 'taluego'
-}
-myStorage.setItem('datajson',JSON.stringify(registro));
-let registrados = myStorage.getItem('registrados')
-
-if (registrados){
-    registrados = JSON.parse(registrados)
-    
-}else{
-    
-}
-
+/* myStorage.setItem('datajson',JSON.stringify(registro)); */
+let registrados = []; 
 function RegistroUsuario(){
-    let usuario = document.getElementById('usuario').value;
+    let usuario = document.getElementById("usuario").value;
     let contrasenia = document.getElementById("contrasenia").value;
     let mail = document.getElementById("mail").value;
     console.log('Registro Completo');
     let registrado = {
-        usuarioR: usuario,
-        contraseniaR: contrasenia,
-        mailR: mail,
+        username: usuario,
+        password: contrasenia,
+        email: mail,
     }
-    registrados.push(registrado)
-    myStorage.setItem('datosPagina', JSON.stringify(this._registrados))
-},
+    registrados.push(registrado);
+    myStorage.setItem('datasson', JSON.stringify(registrados));
+}
 
 
 let Pagina = {
